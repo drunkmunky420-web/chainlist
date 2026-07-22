b13e0a804821b8398bd1715aa8ca6e7929bc30b5
drunkmunky420-web <drunkmunky420@gmail.com>
2026-07-22 14:07:09 -0700

    Update extraRpcs.js
     Add degenprotocol Base RPC and privacy statement

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -1,4 +1,6 @@
 const privacyStatement = {
+    degenprotocol:
+    "We do not track, log, or store user IP addresses, location metadata, or wallet details. Any temporary data is used strictly for service rate limiting and load balancing. https://degenprotocol.io/privacy",
   blockswap:
     "Blockswap RPC does not track any kind of user information at the builder RPC level (i.e. IP, location, etc.) nor is any information logged. All blocks are encrypted when passed between proposers, builders, relayers, and Ethereum. It does not transmit any transactions to the relayer. We use analytical cookies to see which content on the Site is highly frequented and also to analyze if content should be updated or improved. These cookies process and save data like your browser type, referrer URLs, operating system, date/time stamp, views and clicks on the Site, and your (truncated) IP address. For more information please visit: https://docs.pon.network/pon/privacy",
   "48Club":
@@ -5687,6 +5689,11 @@ export const extraRpcs = {
   },
   8453: {
     rpcs: [
+          {
+      url: "https://rpc.degenprotocol.io/base",
+      tracking: "none",
+      trackingDetails: privacyStatement.degenprotocol,
+    },
       {
         url: "https://rpc.nodeflare.app/base/public",
         tracking: "none",
